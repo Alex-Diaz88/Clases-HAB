a3eb9bc84ccd43cccd8cea4e0733150a69f8ad98
AlexDiaz <xandre88@gmail.com>
2022-08-27 21:55:08 +0200

    ejercicio 4 añadido

--- semana-08-react/Ejercicios 1 - 2 - 3 - 4/src/App.js
+++ semana-08-react/Ejercicios 1 - 2 - 3 - 4/src/App.js
@@ -11,6 +11,15 @@ function App() {
       <h1>Tareas</h1>
       <Form tasks={tasks} setTasks={setTasks} />
       <TaskList tasks={tasks} />
+      <section>
+        <h2>Enlaces:</h2>
+        {/* Pensé que tendría que utilizar <img
+        src={process.env.PUBLIC_URL + "/foto.jpg"}
+        alt="foto front-backend"/>  pero funcionó de esta manera...*/}
+        <a href="http://localhost:3000/foto.jpg">Ir a Foto</a>
+
+        <a href={process.env.REACT_APP_DOUBTS}>Necesitas ayuda?</a>
+      </section>
     </div>
   );
 }
